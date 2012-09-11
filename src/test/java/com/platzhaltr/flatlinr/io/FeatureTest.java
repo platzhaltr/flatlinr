9684bbd3fab905dc8426714574dd60bba48abe5b
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-09-11 18:21:59 +0200

    Organize imports

--- src/test/java/com/platzhaltr/flatlinr/io/FeatureTest.java
+++ src/test/java/com/platzhaltr/flatlinr/io/FeatureTest.java
@@ -1,6 +1,6 @@
 package com.platzhaltr.flatlinr.io;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
